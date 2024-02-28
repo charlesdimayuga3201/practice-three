b42f36a58871a2cf010a965ff7ddc8285f245671
Charles Dimayuga <charleskeane3201@gmail.com>
2024-02-28 14:39:16 +0800

    color indicator on box integrate in modal

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ loader.setDRACOLoader(dracoLoader);
 animation;
 loader.load(
   // resource URL
-  "/mets4_racks.glb",
+  "/mets4racks.glb",
   // called when the resource is loaded
   function (gltf) {
     // const root = gltf.scene;
@@ -92,30 +92,69 @@ function onClick(event) {
       object.name === "Box5" ||
       object.name === "Box6"
     ) {
-      object.material.color.set("green");
+      //   object.material.color.set("green");
       //   console.log("color: ", object.material.color);
 
       const colorHex = object.material.color.getHexString();
       const squareId = object.name.replace("Box", "");
-      $(`.square_p${squareId}`).css("background-color", `#${colorHex}`);
+      // $(`.square_p${squareId}`).css("background-color", `#${colorHex}`);
       // Apply this color to corresponding CSS square
 
-      if (object.name === "Box1" || object.name === "Box2") {
-        $(".square_p1,.square_p2").show(); // Show Square 1
+      //   if (object.name === "Box1" || object.name === "Box2") {
+      //     $(".square_p1,.square_p2").show(); // Show Square 1
+      //     if (object.name === "Box1") {
+      //       $(".square_p1").css("background-color", `#${colorHex}`);
+      //     } else if (object.name === "Box2") {
+      //       $(".square_p2").css("background-color", `#${colorHex}`);
+      //     }
+      //   } else if (object.name === "Box3" || object.name === "Box4") {
+      //     $(".square_p1,.square_p2").show(); // Show Square 1
+      //     if (object.name === "Box3") {
+      //       $(".square_p1").css("background-color", `#${colorHex}`);
+      //     } else if (object.name === "Box4") {
+      //       $(".square_p2").css("background-color", `#${colorHex}`);
+      //     }
+      //   } else if (object.name === "Box5" || object.name === "Box6") {
+      //     $(".square_p1,.square_p2").show(); // Show Square 1
+      //     if (object.name === "Box5") {
+      //       $(".square_p1").css("background-color", `#${colorHex}`);
+      //     } else if (object.name === "Box6") {
+      //       $(".square_p2").css("background-color", `#${colorHex}`);
+      //     }
+      //   }
 
-        $(`.square_p${squareId}`).css("background-color", `#${colorHex}`);
-        $(".square_p3,.square_p4,.square_p5,.square_p6").hide(); // Hide Square 2
-      } else if (object.name === "Box3" || object.name === "Box4") {
-        $(".square_p1,.square_p2,.square_p5,.square_p6").hide();
+      // if (
+      //   object.name === "Box1" ||
+      //   object.name === "Box3" ||
+      //   object.name === "Box5"
+      // ) {
+      //   $(".square_p1,.square_p2").show(); // Show Square 1
+      //   $(".square_p1").css("background-color", `#${colorHex}`);
+      // } else if (
+      //   object.name === "Box2" ||
+      //   object.name === "Box4" ||
+      //   object.name === "Box6"
+      // ) {
+      //   $(".square_p1,.square_p2").show(); // Show Square 1
+      //   $(".square_p2").css("background-color", `#${colorHex}`);
+      // }
 
-        $(`.square_p${squareId}`).css("background-color", `#${colorHex}`);
-        $(".square_p3,.square_p4").show();
-      } else if (object.name === "Box5" || object.name === "Box6") {
-        $(".square_p1,.square_p2,.square_p3,.square_p4").hide();
+      //   if (object.name === "Box1" || object.name === "Box2") {
+      //     $(".square_p1,.square_p2").show(); // Show Square 1
 
-        $(`.square_p${squareId}`).css("background-color", `#${colorHex}`);
-        $(".square_p5,.square_p6").show();
-      }
+      //     // $(`.square_p${squareId}`).css("background-color", `#${colorHex}`);
+      //     $(".square_p3,.square_p4,.square_p5,.square_p6").hide(); // Hide Square 2
+      //   } else if (object.name === "Box3" || object.name === "Box4") {
+      //     $(".square_p1,.square_p2,.square_p5,.square_p6").hide();
+
+      //     // $(`.square_p${squareId}`).css("background-color", `#${colorHex}`);
+      //     $(".square_p3,.square_p4").show();
+      //   } else if (object.name === "Box5" || object.name === "Box6") {
+      //     $(".square_p1,.square_p2,.square_p3,.square_p4").hide();
+
+      //     // $(`.square_p${squareId}`).css("background-color", `#${colorHex}`);
+      //     $(".square_p5,.square_p6").show();
+      //   }
 
       showModal(object.name);
     }
@@ -146,7 +185,7 @@ const { ambientLight, mainLight } = createLights();
 
 function showModal(name) {
   $("#modalText").text("Modal should be shown now, name: " + name);
-
+  updateHTMLSquaresForBox(name);
   // Show the modal
   $("#infoModal").css("display", "block");
 
@@ -162,6 +201,96 @@ window.onclick = function (event) {
     $("#infoModal").css("display", "none");
   }
 };
+
+// function updateHTMLSquares() {
+//   scene.traverse(function (child) {
+//     if (child instanceof THREE.Mesh && child.name.startsWith("Box")) {
+//       const colorHex = child.material.color.getHexString();
+//       const squareId = child.name.replace("Box", "");
+//       if (squareId >= 1 && squareId <= 6) {
+//         $(`.square_p${squareId}`).css("background-color", `#${colorHex}`);
+//       }
+//     }
+//   });
+// }
+
+function updateHTMLSquaresForBox(boxName) {
+  const square1Id = "square_p1";
+  const square2Id = "square_p2";
+
+  var colorHex1 = "";
+  let colorHex2 = "";
+
+  switch (boxName) {
+    case "Box1":
+      colorHex1 = getColorHexString("Box1");
+      colorHex2 = getColorHexString("Box2");
+      console.log("Color: ", colorHex1);
+      console.log("Color: ", colorHex2);
+      $(".Box1_content").show();
+      $(
+        ".Box2_content,.Box3_content,.Box4_content,.Box5_content,.Box6_content"
+      ).hide();
+      break;
+    case "Box2":
+      colorHex1 = getColorHexString("Box1");
+      colorHex2 = getColorHexString("Box2");
+      $(".Box2_content").show();
+      $(
+        ".Box1_content,.Box3_content,.Box4_content,.Box5_content,.Box6_content"
+      ).hide();
+      break;
+    case "Box3":
+      colorHex1 = getColorHexString("Box3");
+      colorHex2 = getColorHexString("Box4");
+      $(".Box3_content").show();
+      $(
+        ".Box2_content,.Box1_content,.Box4_content,.Box5_content,.Box6_content"
+      ).hide();
+      break;
+    case "Box4":
+      colorHex1 = getColorHexString("Box3");
+      colorHex2 = getColorHexString("Box4");
+      $(".Box4_content").show();
+      $(
+        ".Box2_content,.Box3_content,.Box1_content,.Box5_content,.Box6_content"
+      ).hide();
+      break;
+    case "Box5":
+      colorHex1 = getColorHexString("Box5");
+      colorHex2 = getColorHexString("Box6");
+      $(".Box5_content").show();
+      $(
+        ".Box2_content,.Box3_content,.Box4_content,.Box1_content,.Box6_content"
+      ).hide();
+      break;
+    case "Box6":
+      colorHex1 = getColorHexString("Box5");
+      colorHex2 = getColorHexString("Box6");
+      $(".Box6_content").show();
+      $(
+        ".Box2_content,.Box3_content,.Box4_content,.Box5_content,.Box1_content"
+      ).hide();
+      break;
+    // Add cases for other boxes if needed
+    default:
+      // Handle other cases or do nothing
+      break;
+  }
+
+  // Update the background colors of the HTML div squares
+  $(`.${square1Id}`).css("background-color", `#${colorHex1}`);
+  $(`.${square2Id}`).css("background-color", `#${colorHex2}`);
+}
+
+function getColorHexString(boxName) {
+  const object = scene.getObjectByName(boxName);
+  console.log("BoxName: ", object);
+  if (object) {
+    return object.material.color.getHexString();
+  }
+  return "";
+}
 
 function animation() {
   scene.add(ambientLight, mainLight);
